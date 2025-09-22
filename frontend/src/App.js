b8a6fa7b021d6538405d094e6904c5abeeaaab00
gandhisaajan <106997558+gandhisaajan@users.noreply.github.com>
2025-09-22 17:44:40 -0400

    Frontend bootstrapped with Tailwind working

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="text-3xl font-bold">Tailwind is working ✅</div>
+    </div>
+  );
+}
+
+export default App;
